b6221242048118c733ceefb9a49e9ffdfba2a845
Brian Schonecker <bschonec@gmail.com>
2015-04-27 19:12:07 -0400

    Removed tabs and cleaned up spacing.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,24 +5,25 @@ class fstab::params {
 case $::osfamily {
   
   'Debian': { 
-
-		$nfs_client_package = ['nfs-common',] 
-		$nfs_server_package = ['nfs-kernel-server',] 
-	} # 'Debian'
+  
+     $nfs_client_package = ['nfs-common',] 
+     $nfs_server_package = ['nfs-kernel-server',] 
+     
+  } # 'Debian'
   
   # Packages for RHEL6 are different than RHEL7
   'RedHat': { 
   
-        $nfs_server_package = $::lsbmajdistrelease ?  {
-          6 =>  ['nfs-utils', 'nfs-utils-lib'],
-          7 =>  ['nfs-utils', 'libnfsidmap'],
-        }    
+     $nfs_server_package = $::lsbmajdistrelease ?  {
+       6 =>  ['nfs-utils', 'nfs-utils-lib'],
+       7 =>  ['nfs-utils', 'libnfsidmap'],
+     }    
  
-        $nfs_client_package = $::lsbmajdistrelease ?  {
-          6 =>  ['nfs-utils', 'nfs-utils-lib'],
-          7 =>  ['nfs-utils', 'libnfsidmap'],
-        }     
-  
+     $nfs_client_package = $::lsbmajdistrelease ?  {
+        6 =>  ['nfs-utils', 'nfs-utils-lib'],
+        7 =>  ['nfs-utils', 'libnfsidmap'],
+      }     
+      
     } # 'RedHat'
 
 } # case
